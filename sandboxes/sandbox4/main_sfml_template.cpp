e5e246e16ea0594e0ec6be1fbb74b1d5c6d1af25
Aleksi <aleksiandeson@kamk.fi>
2022-10-11 15:18:22 +0300

    Moved engine methods to libs.h

--- sandboxes/sandbox4/main_sfml_template.cpp
+++ sandboxes/sandbox4/main_sfml_template.cpp
@@ -5,16 +5,12 @@
 #include <string>
 #include <stdio.h>
 #include <vector>
+#include <libs.h>
 
 using namespace std;
 
 //Constants
-const string levelPath = "levels/";
-const string assetPath = "levels/";
 const float g = .42;
-const int scale = 50;
-const int mapWidth = 20;
-const int mapHeight = 20;
 
 //Player movement Variables
 int moveSpeed = 10;
@@ -22,112 +18,13 @@ float fallSpeed = 0;
 sf::Vector2f deltaPos;
 float jumpPower = 0;
 
-//Mouse button trackers
+//Button trackers
 bool lmbDown, rmbDown, plusDown = false;
-
-class Platform
-{
-public:
-	Platform(sf::Vector2f pos1, sf::Vector2f pos2, sf::Texture platformTexture)
-	{
-		topLeft = sf::Vector2f(min(pos1.x, pos2.x), min(pos1.y, pos2.y));
-		bottomRight = sf::Vector2f(max(pos1.x, pos2.x), max(pos1.y, pos2.y));
-		texture = platformTexture;
-	}
-
-	sf::Texture texture;
-	sf::Vector2f topLeft;
-	sf::Vector2f bottomRight;
-
-	sf::RectangleShape draw()
-	{
-		sf::RectangleShape rectangle;
-		rectangle.setPosition(topLeft);
-		rectangle.setSize(bottomRight - topLeft);
-		rectangle.setTexture(&texture);
-
-		return rectangle;
-	}
-	sf::FloatRect getGlobalBounds()
-	{
-		return sf::FloatRect(topLeft, sf::Vector2f(bottomRight - topLeft));
-	}
-};
-
-//Save 2D vector tilemap to file
-void SaveTilemap(vector<vector<uint8_t>> tilemap, string file)
-{
-	filesystem::create_directory(levelPath);
-	ofstream tilemapSave(levelPath + file);
-
-	for (int x = 0; x < tilemap.size(); x++)
-	{
-		for (int y = 0; y < tilemap[0].size(); y++)
-		{
-			tilemapSave << tilemap[x][y];
-		}
-		tilemapSave << endl;
-	}
-
-	cout << "Saved tilemap to " << levelPath + file << endl;
-	tilemapSave.close();
-}
-
-//Load 2D vector tilemap from file
-vector<vector<uint8_t>> LoadTilemap(string file)
-{
-	ifstream tilemapSave((levelPath + file).c_str());
-
-	vector<vector<uint8_t>> tilemap(mapHeight, vector<uint8_t>(mapWidth, 1));
-	for (string sRow; getline(tilemapSave, sRow);)
-	{
-		vector<uint8_t> row;
-		for (int i = 0; i < sRow.length(); i++)
-		{
-			row.push_back(sRow[i]);
-		}
-		tilemap.push_back(row);
-	}
-
-	tilemapSave.close();
-	return tilemap;
-}
-
-//Load texture with error checking
-sf::Texture loadTexture(string name)
-{
-	sf::Texture texture;
-	if (!texture.loadFromFile(assetPath + name))
-	{
-		printf("Load texture error");
-	}
-	return texture;
-}
-
-//Returns true if any corner of sprite is overlapping a non zero position of the tilemap.
-//Assumes origin of sprite is in the center. Optional offset for shifting the hitbox.
-bool CheckTilemapCollision(sf::Sprite sprite, vector<vector<uint8_t>> tilemap, sf::Vector2f offset = sf::Vector2f(0, 0))
-{
-	sf::Vector2f position = sprite.getPosition() + offset;
-	int width = sprite.getGlobalBounds().width / 2;
-	int height = sprite.getGlobalBounds().height / 2;
-
-	//Check if in bounds
-	if(position.x - width > 0 && position.x + width < mapWidth * scale && position.y + height < mapHeight * scale && position.y - height > 0)
-		//Check if any corner is within the tilemap
-		return (tilemap[(position.y - height) / scale][(position.x - width) / scale] != 0) ||
-			(tilemap[(position.y - height) / scale][(position.x + width) / scale] != 0) ||
-			(tilemap[(position.y + height) / scale][(position.x - width) / scale] != 0) ||
-			(tilemap[(position.y + height) / scale][(position.x + width) / scale] != 0);
-
-	return true;
-}
 
 int main()
 {
 	//Create main level tilemap [ROW][COLUMN]
-	vector<vector<uint8_t>> tilemap(mapHeight, vector<uint8_t>(mapWidth, 1));
-	tilemap = LoadTilemap("map1");
+	vector<vector<uint8_t>> tilemap = LoadTilemap("map1");
 
 	//Load textures
 	sf::Texture texture = loadTexture("image1.png");
@@ -146,14 +43,6 @@ int main()
 	player.setPosition(250, 250);
 
 	sf::Sprite tilemapDrawerSprite(woodTexture);
-
-	//Old variables for drawing platforms
-	sf::Vector2i pos1;
-	sf::Vector2i pos2;
-	sf::Vector2i mousePos;
-	sf::RectangleShape tempPlatform(sf::Vector2f(0, 0));
-	tempPlatform.setTexture(&texture);
-	vector<Platform> platforms;
 
 	//Debug and dev stuff
 	sf::Font arial;
@@ -195,65 +84,6 @@ int main()
 				}
 			}
 		}
-
-		/*
-		//Platform mouse drawing and deleting
-		{
-			//Draw all saved boxes
-			for (int i = 0; i < platforms.size(); i++)
-			{
-				window.draw(platforms[i].draw());
-			}
-
-			//Realtime Draw new box
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
-			{
-				if (!lmbDown)
-				{
-					pos1 = sf::Mouse::getPosition(window);
-					lmbDown = true;
-				}
-				if (lmbDown)
-				{
-					pos2 = sf::Mouse::getPosition(window);
-
-					//Draw the box
-					tempPlatform.setSize(sf::Vector2f(abs(pos1.x - pos2.x), abs(pos1.y - pos2.y)));
-					tempPlatform.setPosition(sf::Vector2f(std::min(pos1.x, pos2.x), std::min(pos1.y, pos2.y)));
-					window.draw(tempPlatform);
-				}
-			}
-			else
-			{
-				if (lmbDown)
-				{
-					lmbDown = false;
-					platforms.push_back(Platform((sf::Vector2f)pos1, (sf::Vector2f)pos2, texture));
-
-					//draw to prevent 1 frame flicker
-					window.draw(platforms[platforms.size() - 1].draw());
-				}
-			}
-
-			//delete with right click
-			if (sf::Mouse::isButtonPressed(sf::Mouse::Right) && !rmbDown)
-			{
-				rmbDown = true;
-				for (int i = platforms.size() - 1; i >= 0; i--)
-				{
-					mousePos = sf::Mouse::getPosition(window);
-					if (mousePos.x > platforms[i].topLeft.x && mousePos.x < platforms[i].bottomRight.x && mousePos.y > platforms[i].topLeft.y && mousePos.y < platforms[i].bottomRight.y)
-					{
-						platforms.erase(platforms.begin() + i);
-						break;
-					}
-				}
-			}
-			if (!sf::Mouse::isButtonPressed(sf::Mouse::Right))
-			{
-				rmbDown = false;
-			}
-		}*/
 
 		//Tilemap design and manipulation
 		{
