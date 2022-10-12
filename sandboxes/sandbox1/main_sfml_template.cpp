4038b476f5c5bc79ffb814af0a81fe8e36c6bace
Lasse Autio <lasseautio@kamk.fi>
2022-10-12 14:20:02 +0300

    Replace main_sfml_template.cpp

--- sandboxes/sandbox1/main_sfml_template.cpp
+++ sandboxes/sandbox1/main_sfml_template.cpp
@@ -1,199 +1,221 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
 #include <SFML/Graphics.hpp>
-#include <stdio.h>
 #include <string>
+#include <stdio.h>
+#include <vector>
+#include <libs.h>
 
-//assetit eri kansiosta riippuen ollaanko debug vai relese k‰‰nnˆksess‰.
-#if defined(_DEBUG)
-std::string ASSET_PATH = "../assets/";
-#else
-std::string ASSET_PATH = "assets/";
-#endif
+using namespace std;
 
-sf::Texture loadTexture(const std::string& fileName) {
-    sf::Texture texture;
-    if (!texture.loadFromFile(ASSET_PATH + fileName)) {
-        // error...
-        printf("Error loading image!\n");
-    }
-    return texture;
-}
+//Constants
+const float g = .42;
 
-class GameObject {
-public:
-    GameObject(float px, float py) 
-        : positionX(px), positionY(py) {
+//Player movement Variables
+int moveSpeed = 10;
+float fallSpeed = 0;
+sf::Vector2f deltaPos;
+float jumpPower = 0;
 
-    }
+//Button trackers
+bool lmbDown, rmbDown, plusDown = false;
 
-    float getX() const {
-        return positionX;
-    }
-    float getY() const {
-        return positionY;
-    }
-
-    void move(float deltaX, float deltaY) {
-        positionX += deltaX;
-        positionY += deltaY;
-    }
-private:
-    float positionX;
-    float positionY;
-};
-
-bool sphereSphereCollisionCheck(const GameObject& o1, const GameObject& o2) {
-    float deltaX = o2.getX() - o1.getX();
-    float deltaY = o2.getY() - o1.getY();
-    float c = abs(deltaX * deltaX + deltaY * deltaY);
-    float r1PlusR2 = 1.0f; // oletetaan, ett‰ peliobjektit ovat ykkˆsen kokoisia, ts r1 = 0,5, r2 = 0,5 -> r1+r2 == 1
-    return c < r1PlusR2;
-
-}
-
-int main() 
+int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
-    std::vector<sf::Texture> textures = {
-        
-        loadTexture("image1.png"),
-        loadTexture("image2.png"),
-        loadTexture("player.png"),
-        loadTexture("enemy.png")// indeksi 2
-    };
+	//Create main level tilemap [ROW][COLUMN]
+	vector<vector<uint8_t>> tilemap = LoadTilemap("map1");
 
-  
-   
-    sf::Clock frameTimer;
+	//Load textures
+	sf::Texture texture = loadTexture("image.png");
+	sf::Texture playerTexture = loadTexture("player.png");
+	sf::Texture woodTexture = loadTexture("wall.png");
+	sf::Texture wood2Texture = loadTexture("wall2.png");
 
-    // 8-bit value to describe, what kind of game object os oin location x,y.
-    // mapp‰‰ x,y koordinaatit 8 bit unsigned integeriksi.
-    const float  SCALE = 64.0f;
-    std::vector< std::vector<uint8_t> > map = {
-        {1, 1, 1, 1},
-        {1, 0, 0, 1},
-        {1, 0, 0, 1},
-        {1, 1, 1, 1}
-        
-    };
+	//Add map textures to list
+	vector<sf::Texture> textures;
+	textures.push_back(woodTexture);
+	textures.push_back(wood2Texture);
 
-    std::vector<GameObject> players = {
-        GameObject(0,0)
-    };
+	//Create player
+	sf::Sprite player(playerTexture);
+	player.setOrigin(player.getGlobalBounds().height / 2, player.getGlobalBounds().width / 2);
+	player.setPosition(250, 250);
 
-    std::vector<GameObject> enemies = {
-        GameObject(4,2)
-    };
+	sf::Sprite tilemapDrawerSprite(woodTexture);
 
+	//Debug and dev stuff
+	sf::Font arial;
+	arial.loadFromFile(assetPath + "\\fonts\\ARIAL.TTF");
+	sf::Text debugText;
+	debugText.setFont(arial);
+	debugText.setCharacterSize(25);
 
+	//Create the window
+	sf::RenderWindow window(sf::VideoMode(mapWidth * scale, mapHeight * scale), "Game");
+	window.setFramerateLimit(60);
 
-    sf::Sprite mySprite(textures[0]);
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
+		// clear the window with black color
+		window.clear(sf::Color::Black);
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
+		//Draw based on map
+		for (size_t x = 0; x < tilemap.size(); x++)
+		{
+			for (size_t y = 0; y < tilemap[x].size(); y++)
+			{
+				auto spriteType = tilemap[x][y];
+				if (spriteType != 0)
+				{
+					tilemapDrawerSprite.setTexture(textures[spriteType - 1]);
+					tilemapDrawerSprite.setPosition(y * scale, x * scale);
+					window.draw(tilemapDrawerSprite);
+				}
+			}
+		}
 
+		//Tilemap design and manipulation
+		{
+			//Add with left click
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				if (sf::Mouse::getPosition(window).y / scale < tilemap.size() && sf::Mouse::getPosition(window).x / scale < tilemap[0].size())
+					tilemap[sf::Mouse::getPosition(window).y / scale][sf::Mouse::getPosition(window).x / scale] = 1;
+			}
 
-        }
-        float deltaTime= frameTimer.getElapsedTime() .asSeconds();
-        frameTimer.restart();
-        // clear the window with black color
-        window.clear(sf::Color::Black);
+			//Clear with right click
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+			{
+				if (sf::Mouse::getPosition(window).y / scale < tilemap.size() && sf::Mouse::getPosition(window).x / scale < tilemap[0].size())
+					tilemap[sf::Mouse::getPosition(window).y / scale][sf::Mouse::getPosition(window).x / scale] = 0;
+			}
 
-        // 1. read input
-        float deltaX = 0.0f;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        {
-            // left key is pressed: move our character
-            deltaX -= 2.0f * deltaTime;
-        }
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add))
+			{
+				if (!plusDown)
+				{
+					plusDown = true;
+					SaveTilemap(tilemap, "map1");
+				}
+			}
+			else
+			{
+				plusDown = false;
+			}
+		}
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-        {
-            // left key is pressed: move our character
-            deltaX += 2.0f * deltaTime;
-        }
+		//Player movement
+		{
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			{
+				if (!CheckTilemapCollision(player, tilemap, sf::Vector2f(-1 * moveSpeed, 0)))
+				{
+					deltaPos.x += -1 * moveSpeed;
+				}
+				else
+				{
+					for (int i = 2; i < 4; i++)
+					{
+						if (!CheckTilemapCollision(player, tilemap, sf::Vector2f((-1 * moveSpeed) / i, 0)))
+						{
+							deltaPos.x += (-1 * moveSpeed) / i;
+						}
+					}
+				}
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			{
+				if (!CheckTilemapCollision(player, tilemap, sf::Vector2f(1 * moveSpeed, 0)))
+				{
+					deltaPos.x += 1 * moveSpeed;
+				}
+				else
+				{
+					for (int i = 2; i < 4; i++)
+					{
+						if (!CheckTilemapCollision(player, tilemap, sf::Vector2f((1 * moveSpeed) / i, 0)))
+						{
+							deltaPos.x += (1 * moveSpeed) / i;
+						}
+					}
+				}
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+			{
+				if (!CheckTilemapCollision(player, tilemap, sf::Vector2f(0, -1 * jumpPower)))
+				{
+					deltaPos.y += -1 * jumpPower;
+					if (jumpPower > 0)
+						jumpPower -= g;
+					else
+						jumpPower = 0;
+				}
+				else
+				{
+					for (int i = 2; i < 4; i++)
+					{
+						if (!CheckTilemapCollision(player, tilemap, sf::Vector2f(0, (-1 * jumpPower) / i)))
+						{
+							deltaPos.y += (-1 * jumpPower) / i;
+						}
+						else
+						{
+							fallSpeed = 0;
+							jumpPower = 0;
+						}
+					}
+				}
+			}
+			else
+			{
+				jumpPower = 0;
+			}
+		}
 
-        float deltaY = 0.0f;
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            // down key is pressed: move our character
-            deltaY -= 2.0f * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-         
-        {
-           // up key is pressed: move our character
-            deltaY += 2.0f * deltaTime;
-        }
-        
-        // 2. Move game objects according to input (update)
+		//Gravity Physics
+		if (!CheckTilemapCollision(player, tilemap, sf::Vector2f(0, max(fallSpeed, 0.01f))))
+		{
+			deltaPos.y += fallSpeed;
+			if (fallSpeed < 20)
+				fallSpeed += g;
+		}
+		else
+		{
+			for (int i = 2; i < 16; i++)
+			{
+				if (!CheckTilemapCollision(player, tilemap, sf::Vector2f(0, 2 / i)))
+				{
+					deltaPos.y += 2 / i;
+				}
+				else
+				{
+					fallSpeed = 0;
+					jumpPower = 15;
+				}
+			}
+		}
 
-        players[0].move(deltaX, deltaY);
-        
+		player.move(deltaPos);
+		deltaPos = sf::Vector2f();
 
+		// draw everything here
+		window.draw(player);
+		window.draw(debugText);
 
-        // Check collisions
-        for (size_t playerIndex = 0; playerIndex < players.size(); ++playerIndex) {
-            for (size_t enemyIndex = 0; enemyIndex < enemies.size(); ++enemyIndex) {
-                if (sphereSphereCollisionCheck(players[playerIndex], enemies[enemyIndex])) {
-                    printf("Player: %d collides with enemy: %d\n", (int)playerIndex, (int)enemyIndex);
-                }
-            }
-        }
+		// end the current frame
+		window.display();
+	}
 
-        // Read input
-        // mySprite.setScale(0.1f, 0.1f);
-        // mySprite.setRotation(45.0f); 
-        // mySprite.rotate(360.0f * deltaTime);
-        // mySprite.setPosition(200.0f, 100.0f);
-
-        // 3. "render"
-        // Piirr‰ sprite ruudulle
-        // window.draw(mySprite);
-        // piirr‰ ensin map ruudulle
-
-
-        for (size_t y = 0; y < map.size(); ++y) {
-            for (size_t x = 0; x < map[y].size(); ++x) {
-                auto spriteType = map[y][x];
-                sf::Sprite mySprite(textures[spriteType]);
-                mySprite.setPosition(SCALE*x, SCALE*y);
-                window.draw(mySprite);
-            }
-        }
-
-        // piirr‰ sitten playerit ruudulle
-        for (size_t i = 0; i < players.size(); ++i) {
-            auto spriteType = 2; // Player sprite indeksi 2
-            sf::Sprite mySprite(textures[spriteType]);
-            auto& player = players[i];
-            mySprite.setPosition(SCALE * player.getX(), SCALE * player.getY());
-            window.draw(mySprite);
-        }
-            // piirr‰ sitten enemyt ruudulle
-            for (size_t i = 0; i < enemies.size(); ++i) {
-                auto spriteType = 3; // Enemy sprite indeksi 3
-                sf::Sprite mySprite(textures[spriteType]);
-                auto& enemy = enemies[i];
-                mySprite.setPosition(SCALE * enemy.getX(), SCALE * enemy.getY());
-                window.draw(mySprite);
-        }
-
-        
-        // 4: end the current frame (swap buffers)
-        window.display();
-
-      
-    }
-
-    return 0;
+	return 0;
 }
