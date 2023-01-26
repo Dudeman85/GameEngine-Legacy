a0dd53dbdc882fb8c9908895e6bbb33ec9e6fdd1
Anderson Aleksi TTV21SP <aleksianderson@kamk.fi>
2023-01-26 13:12:55 +0200

    Added GLFW and GLAD

--- sandboxes/sandbox5/main_sfml_template.cpp
+++ sandboxes/sandbox5/main_sfml_template.cpp
@@ -1,35 +1,100 @@
-#include "engine/Application.h"
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-ECS ecs;
+//Resize window callback
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
 
-using namespace engine;
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 
 int main()
 {
-	EngineLib lib;
+    //Initialize GLFW and set it to require OpenGL 3
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	Entity player = ecs.newEntity();
-	ecs.addComponent(player, Transform{ .x = 100, .y = 100 });
-	
+    //Create window object
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Window", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-	//SFML window
-	sf::RenderWindow window(sf::VideoMode(800, 600), "test");
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
-	//Main game loop
-	while (window.isOpen())
-	{
-		//SFML window close event
-		sf::Event event;
-		while (window.pollEvent(event))
-			if (event.type == sf::Event::Closed)
-				window.close();
-		window.clear(sf::Color::Black);
+    const char* vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Fuck you, dumbass:\n" << infoLog << std::endl;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    //Set the resize window callback function
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
-		lib.transformSystem->Update();
-		lib.renderSystem->Update(window);
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
 
-		//SFML display window
-		window.display();
-	}
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+
+    //Main Loop
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        //Set the clear color to black and clear the screen
+        glClearColor(0, 0, 0, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
 }
