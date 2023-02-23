488bfa0b150c98252c4db9dded36dd3067b26cd0
Viksteri <viksterikappi@kamk.fi>
2023-02-23 14:53:55 +0200

    Added libsndfile and created working sound system in sandbox1

--- sandboxes/sandbox1/main_sfml_template.cpp
+++ sandboxes/sandbox1/main_sfml_template.cpp
@@ -1,6 +1,19 @@
-#include <stb_image.h>
+#include "SoundDevice.h"
+#include "SoundBuffer.h"
+#include "SoundSource.h"
+#include <iostream>
 
 int main()
 {
+	std::cout << "starting...\n";
+
+	SoundDevice* mysounddevice = SoundDevice::get();
+	uint32_t sound1 = SoundBuffer::get()->addSoundEffect("C:/Users/viksterikap/Desktop/projekti/tiimi4/sandboxes/sandbox1/spring-weather-1.wav");
+	uint32_t sound2 = SoundBuffer::get()->addSoundEffect("C:/Users/viksterikap/Desktop/projekti/tiimi4/sandboxes/sandbox1/jump.wav");
+	SoundSource mySpeaker;
+	mySpeaker.Play(sound1);
+	mySpeaker.Play(sound2);
+	std::cout << "got here\n";
+
 	return 0;
 }
