3117b092d672a51bf53ea5c7fc5072bc109727b5
Viksteri Käppi <viksterikappi@kamk.fi>
2023-02-26 21:11:34 +0200

    Added music loader

--- sandboxes/sandbox1/main_sfml_template.cpp
+++ sandboxes/sandbox1/main_sfml_template.cpp
@@ -1,5 +1,6 @@
 #include "SoundDevice.h"
 #include "SoundBuffer.h"
+#include "MusicBuffer.h"
 #include "SoundSource.h"
 #include <iostream>
 
@@ -8,11 +9,21 @@ int main()
 	std::cout << "starting...\n";
 
 	SoundDevice* mysounddevice = SoundDevice::get();
-	uint32_t sound1 = SoundBuffer::get()->addSoundEffect("assets/spring-weather-1.wav");
-	uint32_t sound2 = SoundBuffer::get()->addSoundEffect("assets/jump.wav");
+	uint32_t sound1 = SoundBuffer::get()->addSoundEffect("assets/jump.wav");
+	
+
 	SoundSource mySpeaker;
-	mySpeaker.Play(sound2);
+
 	mySpeaker.Play(sound1);
+
+	MusicBuffer myMusic("assets/spring-weather-1.wav");
+	myMusic.Play();
+
+	while (1)
+	{
+		myMusic.updateBufferStream();
+	}
+
 	std::cout << "got here\n";
 
 	return 0;
