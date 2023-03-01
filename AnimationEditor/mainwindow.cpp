a591be8198ca40c21b14dfa4b296cd11691c6087
Anderson Aleksi TTV21SP <aleksianderson@kamk.fi>
2023-03-01 15:53:50 +0200

    Almost done with the animation editor

--- AnimationEditor/mainwindow.cpp
+++ AnimationEditor/mainwindow.cpp
@@ -4,6 +4,9 @@
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->delay->setValidator( new QIntValidator(1, 100000000, this) );
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &MainWindow::AnimationLoop);
 }
 
 MainWindow::~MainWindow()
@@ -11,6 +14,38 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//Updates all GUI elements
+void MainWindow::UpdateDisplay()
+{
+    if(frames > 0)
+    {
+        ui->mainScene->setPixmap(sprites[frameSprites[currentFrame]]);
+        ui->frameSlider->setMaximum(std::max(0, frames - 1));
+        ui->frameSlider->setSliderPosition(currentFrame);
+        ui->delay->setText(QString::number(frameDelays[currentFrame]));
+    }
+    else
+    {
+        ui->mainScene->clear();
+    }
+}
+
+void MainWindow::AnimationLoop()
+{
+    animationMsElapsed++;
+
+    if(animationMsElapsed >= frameDelays[currentFrame])
+    {
+        animationMsElapsed = 0;
+        currentFrame++;
+        if(currentFrame >= frames)
+            currentFrame = 0;
+
+        UpdateDisplay();
+    }
+}
+
+//Load sprite images
 void MainWindow::on_actionOpen_triggered()
 {
     //Create the select files dialog
@@ -31,21 +66,119 @@ void MainWindow::on_actionOpen_triggered()
         sprites.push_back(image);
     }
 
-    for (QPixmap sprite : ui->spriteGrid)
+    //Delete all the old labels
+    for (ClickableLabel* label : spriteLabels)
     {
-
+        delete label;
     }
+    spriteLabels.clear();
 
+    //TODO Alogrithm
     int maxRows = 3 + sprites.size() * 0.2;
     //Display all the sprites in a grid
     for(uint i = 0; i < sprites.size(); i++)
     {
-        QLabel* spriteLabel = new QLabel(this);
+        //Make the clickable label for displaying the sprites
+        ClickableLabel* spriteLabel = new ClickableLabel(this);
+        spriteLabel->ID = i;
         spriteLabel->setPixmap(sprites[i]);
         spriteLabel->setScaledContents( true );
         spriteLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+        connect(spriteLabel, SIGNAL(clicked()), this, SLOT(onSpriteClicked()));
 
+        //Store the labels so they can be deleted
+        spriteLabels.push_back(spriteLabel);
+
+        //Add the label to the display grid
         ui->spriteGrid->addWidget(spriteLabel, i % maxRows, i / maxRows);
     }
+
+    spriteLabels[selectedSprite]->setStyleSheet("border: 2px solid black;");
+}
+
+//Select the sprite when clicked
+void MainWindow::onSpriteClicked()
+{
+    ClickableLabel *label = (ClickableLabel*)sender();
+
+    //Remove the previously selected sprite border
+    spriteLabels[selectedSprite]->setStyleSheet("border: 0px solid black;");
+
+    //Add a border and select the clicked sprite
+    label->setStyleSheet("border: 2px solid black;");
+    selectedSprite = label->ID;
+}
+
+//When add frame butten is clicked
+void MainWindow::on_addFrame_clicked()
+{
+    //If no sprites have been loaded do nothing
+    if(sprites.empty())
+        return;
+
+    currentFrame++;
+    frameSprites.insert(frameSprites.begin() + currentFrame, selectedSprite);
+    frameDelays.insert(frameDelays.begin() + currentFrame, ui->delay->text().toInt());
+    frames++;
+
+    qDebug() << frameSprites;
+    qDebug() << frameDelays;
+    UpdateDisplay();
+}
+void MainWindow::on_removeFrame_clicked()
+{
+    //If no frames have been added do nothing
+    if(frames == 0)
+        return;
+
+    frameSprites.erase(frameSprites.begin() + currentFrame);
+    frameDelays.erase(frameDelays.begin() + currentFrame);
+    frames--;
+    if(currentFrame > 0)
+        currentFrame--;
+    if(frames == 0)
+        currentFrame = -1;
+
+    UpdateDisplay();
+}
+void MainWindow::on_delay_editingFinished()
+{
+    //If no frames have been added do nothing
+    if(frameDelays.empty())
+        return;
+
+    frameDelays[currentFrame] = ui->delay->text().toInt();
+}
+
+//Timeline control buttons and slider
+void MainWindow::on_backwards_clicked()
+{
+    if(currentFrame > 0)
+        currentFrame--;
+    UpdateDisplay();
+}
+void MainWindow::on_forwards_clicked()
+{
+    if(currentFrame < frames - 1)
+        currentFrame++;
+    UpdateDisplay();
+}
+void MainWindow::on_frameSlider_sliderMoved(int position)
+{
+    currentFrame = position;
+    UpdateDisplay();
+}
+void MainWindow::on_play_clicked()
+{
+    //If less than 2 frames have been added do nothing
+    if(frames < 2)
+        return;
+
+    if(!playingAnimation)
+        timer->start(1);
+    else
+        timer->stop();
+
+    playingAnimation = !playingAnimation;
 }
 
--- AnimationEditor/mainwindow.h
+++ AnimationEditor/mainwindow.h
@@ -6,6 +6,11 @@
 #include <QMainWindow>
 #include <QLabel>
 #include <QFileDialog>
+#include <QGraphicsScene>
+#include <QIntValidator>
+#include <QTimer>
+
+#include "clickablelabel.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,13 +23,35 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void UpdateDisplay();
+    void AnimationLoop();
 
 private slots:
+    void onSpriteClicked();
     void on_actionOpen_triggered();
+    void on_addFrame_clicked();
+    void on_frameSlider_sliderMoved(int position);
+    void on_backwards_clicked();
+    void on_forwards_clicked();
+    void on_delay_editingFinished();
+    void on_removeFrame_clicked();
+    void on_play_clicked();
 
 private:
+    QTimer *timer;
     Ui::MainWindow *ui;
     std::vector<QPixmap> sprites;
+    std::vector<ClickableLabel*> spriteLabels;
 
+    bool playingAnimation;
+    int animationMsElapsed;
+
+    int selectedSprite = 0;
+    int currentFrame = -1;
+    int frames = 0;
+    //ID of the sprite for each frame
+    std::vector<int> frameSprites;
+    //Delays after each frame in ms
+    std::vector<int> frameDelays;
 };
 #endif // MAINWINDOW_H
