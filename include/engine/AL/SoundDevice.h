4e5728607229e628f7b2e197200de1ebb5f433cd
Viksteri <viksterikappi@kamk.fi>
2023-03-17 14:28:04 +0200

    More features to musicplayer, changes to sound effects

--- include/engine/AL/SoundDevice.h
+++ include/engine/AL/SoundDevice.h
@@ -4,17 +4,14 @@
 class SoundDevice
 {
 public:
-	static SoundDevice* getDevice();
+	static SoundDevice* get();
 	static void Init();
 
 	void GetLocation(float& x, float& y, float& z);
-	void GetSourceLocation(float& x, float& y, float& z);
 	void GetOrientation(float& ori);
 	float GetGain();
-	void SetDistance(int key);
 
 	void SetLocation(const float& x, const float& y, const float& z);
-	void SetSourceLocation(const float& x, const float& y, const float& z);
 	void SetOrientation(const float& horx, const float& hory, const float& horz,
 		const float& verx, const float& very, const float& verz);
 	void SetGain(const float& val);
---
+++ include/engine/AL/SoundSource.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <AL/al.h>
+class SoundSource
+{
+public:
+	SoundSource();
+	~SoundSource();
+
+	void Play(const ALuint& buffer_to_play);
+	void Stop();
+	void Pause();
+	void Resume();
+
+	void SetBufferToPlay(const ALuint& buffer_to_play);
+	void SetLooping(const bool& loop);
+
+	bool isPlaying();
+
+private:
+	ALuint p_Source;
+	ALuint p_Buffer = 0;
+};
