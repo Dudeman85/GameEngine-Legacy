106a07617512fb2ed116d18cc4a854499e08f5b9
Viksteri <viksterikappi@kamk.fi>
2023-03-24 13:09:16 +0200

    edits to sound sources

--- sandboxes/sandbox1/SoundSource.cpp
+++ sandboxes/sandbox1/SoundSource.cpp
@@ -3,13 +3,13 @@
 
 SoundSource::SoundSource()
 {
-	alGenSources(1, &p_Source);
+	alGenSources(3, &p_Source);
 	alSourcei(p_Source, AL_BUFFER, p_Buffer);
 }
 
 SoundSource::~SoundSource()
 {
-	alDeleteSources(1, &p_Source);
+	alDeleteSources(3, &p_Source);
 }
 
 void SoundSource::Play(const ALuint& buffer_to_play)
