9a99d116e4595caa5ea539b35d940dd61bc20392
Viksteri <viksterikappi@kamk.fi>
2023-03-24 13:10:17 +0200

    .

--- sandboxes/sandbox1/SoundSource.cpp
+++ sandboxes/sandbox1/SoundSource.cpp
@@ -3,13 +3,14 @@
 
 SoundSource::SoundSource()
 {
-	alGenSources(3, &p_Source);
+
+	alGenSources(1, &p_Source);
 	alSourcei(p_Source, AL_BUFFER, p_Buffer);
 }
 
 SoundSource::~SoundSource()
 {
-	alDeleteSources(3, &p_Source);
+	alDeleteSources(1, &p_Source);
 }
 
 void SoundSource::Play(const ALuint& buffer_to_play)
