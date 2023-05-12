8868f4a6a930bed5dac5cb4239264318cbadb4d5
Viksteri <viksterikappi@kamk.fi>
2023-05-12 12:51:53 +0300

    animator testing

--- sandboxes/sandbox3/main.cpp
+++ sandboxes/sandbox3/main.cpp
@@ -15,25 +15,6 @@ int main()
 	//changes window backround color, values 0-1, RGB
 	
 
-	//Load a new image
-	Image testImage("assets/image.png");
-	//Get the pxel at position (x, y)
-	Pixel pixel = testImage[10][10];
-
-	//Pixel members
-	pixel.r;
-	pixel.g;
-	pixel.b;
-	pixel.a;
-	//Get a slice of testImage from (x1, y1) to (x2, y2). Both are inclusive
-	Image slice = testImage.Slice(0, 0, 30, 30);
-
-
-	//Load a texture from file
-	Texture texture("assets/gradient.png", GL_NEAREST);
-	//Create a texture from an image
-	Texture texture2(testImage, GL_LINEAR);
-
 	//Create a new shader by loading the fragment and vertex shaders from file
 	//Leaving either of these empty will use the default shader for that type
 	Shader shader = Shader("assets/vertexShader.glsl", "assets/fragmentShader.glsl");
@@ -42,49 +23,25 @@ int main()
 	//Create a new entity
 	Entity sprite = ecs.newEntity();
 	
-	//
-	//TÄMÄ PUUTTUU OHJEESTA
 	ecs.addComponent(sprite, Transform{ .x = 300, .y = 200, .xScale = 20, .yScale = 20 });
-	//
 	
-	Sprite& spriteComponent = ecs.getComponent<Sprite>(sprite);
+	
 
 	//Add the sprite component with a texture and custom shader
-	ecs.addComponent(sprite, Sprite{ &texture});
-	//Add the component to an entity
-	Entity player = ecs.newEntity();
-	struct Position
-	{
-		float x, y, z;
-	};
-
-
-	//Create multiple textures from a spritesheet.
-	vector<Texture*> slicedTextures = SliceSpritesheet("spritesheet.png", 4, 6);
-	//Copy the first row of the spritesheet to a new vector
-	vector<Texture*> textures(4);
-	copy(slicedTextures.begin(), slicedTextures.begin() + 4, textures.begin());
-	//Create a vector of delays with each delay being 250ms
-	vector<int> delays(4, 250);
-
-	//Create the animation
-	Animation anim = Animation(textures, delays);
-
-	//Add the Animator component to the sprite entity
+	Sprite& spriteComponent = ecs.addComponent(sprite, Sprite{});
 	ecs.addComponent(sprite, Animator{});
+	
 
-	//Add the anim animation with the name "Animation 1" to the sprite entity
-	AnimationSystem::AddAnimation(sprite, anim, "Animation 1");
 
 
 	//Create animations from a spritesheet where one animation contains all the textures from one row of the spritesheet
 	//The parameters are: the path to the image, how many sprites wide, how many sprites tall, and a vector of delays for each frame
-	std::vector<Animation> anims = AnimationsFromSpritesheet("spritesheet.png", 4, 6, vector<int>(24, 250));
-
+	std::vector<Animation> anims = AnimationsFromSpritesheet("assets/warriorsheet.png", 8, 4, vector<int>(32, 250));
+	
 	//Add the first animation, aka the first row, created from the spritesheet
 	//This has essentially the same results as the above example
-	AnimationSystem::AddAnimation(sprite, anims[0], "Animation 2");
-
+	AnimationSystem::AddAnimation(sprite, anims[0], "Animation 1");
+/*
 	//Start playing an animation named "Animation 1", without looping
 	AnimationSystem::PlayAnimation(sprite, "Animation 1", false);
 	//Stop playing the currently playing animation
@@ -100,21 +57,16 @@ int main()
 	animator.repeatAnimation;
 	animator.playingAnimation;
 	animator.animationTimer;
-
+	
 	//These are equivalent
-	AnimationSystem::AddAnimation(sprite, anim, "Animation 1");
-	engine.animationSystem->AddAnimation(sprite, anim, "Animation 1");
+	AnimationSystem::AddAnimation(sprite, anims, "Animation 1");
+	engine.animationSystem->AddAnimation(sprite, anims, "Animation 1");
+*/
 
 
 
 
 
-	//Register custom component
-	ecs.registerComponent<Position>();
-
-	ecs.addComponent(player, Position{ .x = 10.0f, .y = 25.25f, .z = 0.0f });
-	//Get the Position component of player
-	Position& position = ecs.getComponent<Position>(player);
 	RenderSystem::SetBackgroundColor(.6, .5, .1);
 
 
