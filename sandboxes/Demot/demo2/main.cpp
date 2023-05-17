a04ab0f9af34af2474e4f180e770ac3ff710ecfe
Viksteri <viksterikappi@kamk.fi>
2023-05-17 14:03:01 +0300

    edits to demo2

--- sandboxes/Demot/demo2/main.cpp
+++ sandboxes/Demot/demo2/main.cpp
@@ -13,7 +13,14 @@ ECS ecs;
 
 int main()
 {
-	/*
+
+	//Create the window and OpenGL context before creating EngineLib
+	GLFWwindow* window = CreateWindow(800, 600, "Window");
+
+
+	//Initialize the default engine library
+	EngineLib engine;
+
 	ecs.registerComponent<Player>();
 	shared_ptr<PlayerController> playerController = ecs.registerSystem<PlayerController>();
 	Signature playerControllerSignature;
@@ -23,64 +30,41 @@ int main()
 	playerControllerSignature.set(ecs.getComponentId<Rigidbody>());
 	playerControllerSignature.set(ecs.getComponentId<BoxCollider>());
 	ecs.setSystemSignature<PlayerController>(playerControllerSignature);
-	*/
-	//Create the window and OpenGL context before creating EngineLib
-	GLFWwindow* window = CreateWindow(800, 600, "Window");
-
-	//Initialize the default engine library
-	EngineLib engine;
-
-	engine.physicsSystem->gravity = Vector2(0, -400);
+	
+	engine.physicsSystem->gravity = Vector2(0, 0);
 	engine.physicsSystem->step = 4;
 
 	//Create the camera
 	Camera cam = Camera(800, 600);
-#if 0
-	//Load a new texture
-	Texture texture = Texture("strawberry.png");
+
+	Texture texture = Texture("assets/strawberry.png");
+	Texture texture2 = Texture("assets/crosshairEdit.png");
+	Texture texture3 = Texture("assets/bullet.png");
 
 	//Create a new entity
 	Entity player = ecs.newEntity();
-	ecs.addComponent(player, Transform{ .x = 0, .y = 0 ,.z = 1, .xScale = 20, .yScale = 20 });
+	Transform& playerTransform = ecs.addComponent(player, Transform{ .x = 10, .y = 10, .z = 0, .xScale = 20, .yScale = 20 });
 	ecs.addComponent(player, Sprite{});
-	ecs.addComponent(player, Animator{});
-	ecs.addComponent(player, Rigidbody{ .gravityScale = 0, .drag = 0, .friction = 0, .elasticity = 0 });
+	//ecs.addComponent(player, Animator{});
+	ecs.addComponent(player, Rigidbody{ .gravityScale = 1, .drag = 0, .friction = 0.0, .elasticity = 0 });
 	ecs.addComponent(player, BoxCollider{});
-	BoxCollider& collider = ecs.getComponent<BoxCollider>(player);
+	ecs.addComponent(player, Player{});
+	BoxCollider& playerCollider = ecs.getComponent<BoxCollider>(player);
+	Rigidbody& playerRigidbody = ecs.getComponent<Rigidbody>(player);
 
-	//Define the test animation
-	Animator& animator = ecs.getComponent<Animator>(player);
-	auto testAnims = AnimationsFromSpritesheet("gradient.png", 2, 2, vector<int>(4, 200));
-	AnimationSystem::AddAnimation(player, testAnims[0], "1");
-	AnimationSystem::AddAnimation(player, testAnims[1], "2");
-	AnimationSystem::PlayAnimation(player, "2", true);
+	// create entity crosshair for gamepad
+	Entity crosshair = ecs.newEntity();
+	// adds crosshair texture
+	Transform crosshairTransform = ecs.addComponent(crosshair, Transform{ .x = 500, .y = 500, .xScale = 20, .yScale = 20 });
+	ecs.addComponent(crosshair, Sprite{ &texture2 });
 
-	//Top-Right
-	Entity sprite2 = ecs.newEntity();
-	ecs.addComponent(sprite2, Transform{ .x = 300, .y = 200, .xScale = 20, .yScale = 20 });
-	ecs.addComponent(sprite2, Sprite{ &texture });
-	ecs.addComponent(sprite2, Rigidbody{ .kinematic = true });
-	ecs.addComponent(sprite2, BoxCollider{ .scale = Vector2(10, 1) });
-	//Bottom-Left
-	Entity sprite3 = ecs.newEntity();
-	ecs.addComponent(sprite3, Transform{ .x = -300, .y = -200, .xScale = 20, .yScale = 20 });
-	ecs.addComponent(sprite3, Sprite{ &texture });
-	ecs.addComponent(sprite3, Rigidbody{ .kinematic = true });
-	ecs.addComponent(sprite3, BoxCollider{});
-	//Top-Left
-	Entity sprite4 = ecs.newEntity();
-	ecs.addComponent(sprite4, Transform{ .x = -310, .y = 200, .xScale = 20, .yScale = 20 });
-	ecs.addComponent(sprite4, Sprite{ &texture });
-	ecs.addComponent(sprite4, Rigidbody{ .drag = 0.1, .friction = 0.2, .elasticity = 0.125, .kinematic = false });
-	ecs.addComponent(sprite4, BoxCollider{});
-	//Bottom-Right
-	Entity sprite5 = ecs.newEntity();
-	ecs.addComponent(sprite5, Transform{ .x = 300, .y = -200, .xScale = 20, .yScale = 20 });
-	ecs.addComponent(sprite5, Sprite{ &texture });
-	ecs.addComponent(sprite5, Rigidbody{ .velocity = Vector2(-985, 1000), .drag = 0.25, .elasticity = 0.125, .kinematic = false });
-	ecs.addComponent(sprite5, BoxCollider{});
-#endif
-	RenderSystem::SetBackgroundColor(1.0f, 0.0f, .1);
+	vector <Entity>bullets;
+
+
+	float fireCooldown = 0.4f;
+	bool canFire = true;
+
+	RenderSystem::SetBackgroundColor(0.3f, 0.3f, 0.1f);
 	Tilemap map(&cam);
 	map.loadMap("assets/Test.tmx");
 	//map.position = glm::vec3(500.0f, 0.0f, 0.0f);
@@ -90,49 +74,132 @@ int main()
 	//Game Loop
 	while (!glfwWindowShouldClose(window))
 	{
-		//std::this_thread::sleep_for(std::chrono::milliseconds{100});
+		
 		
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, true);
-		
 
-#if 0
-		//test movement
-		if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-		{
-			engine.physicsSystem->Move(player, Vector2(500, 0) * engine.deltaTime);
-		}
-		if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-		{
-			engine.physicsSystem->Move(player, Vector2(-500, 0) * engine.deltaTime);
-		}
-		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		{
-			engine.physicsSystem->Move(player, Vector2(0, 500) * engine.deltaTime);
-		}
-		if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		{
-			engine.physicsSystem->Move(player, Vector2(0, -500) * engine.deltaTime);
-		}
+		playerController->Update(window, engine.deltaTime, engine.physicsSystem);
 
-		Transform playerTransform = ecs.getComponent<Transform>(player);
-		cam.SetPosition(playerTransform.x, playerTransform.y, playerTransform.z + 100);
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		/////////////////OHJAINSÄÄDÖT////////////////////////////////////////////////////
+		int present = glfwJoystickPresent(GLFW_JOYSTICK_1);
+		if (present == GLFW_TRUE) {
 
-		if (collider.collisions.size() > 0)
-		{
-			for (const Collision& c : collider.collisions)
+			GLFWgamepadstate state;
+
+			// set player speed
+			float playerSpeed = 500.0f;
+			//define deadzone
+			float deadzoneSize = 0.5f;
+			//calculate deadzone barrier
+			float deadzoneTreshold = deadzoneSize / 2.0f;
+			//Camera max distance
+			float max_distance = 30.0f;
+
+
+			int axisCount;
+			int buttonCount;
+			float radian = 5.0f;
+
+			const float* axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axisCount);
+			const unsigned char* buttons = glfwGetJoystickButtons(GLFW_JOYSTICK_1, &buttonCount);
+
+
+			if (abs(axes[0]) > deadzoneTreshold || abs(axes[1]) > deadzoneTreshold)
 			{
-				cout << c.a << " " << c.b << endl;
+				// Sets movement vector based on movement speed
+				Vector2 movement(axes[0] * playerSpeed, -axes[1] * playerSpeed);
+
+				// move player based on movement vector
+				engine.physicsSystem->Move(player, movement * engine.deltaTime);
+
 			}
+
+			// sets crosshair position to zero
+			Vector2 crosshairPosition(0, 0);
+			if (abs(axes[2]) > deadzoneTreshold || abs(axes[3]) > deadzoneTreshold)
+			{
+				// Changes joystick cordinates into origo and scales it between -1, 1
+				Vector2 rightThumbstick(axes[2], axes[3]);
+				rightThumbstick = (rightThumbstick + Vector2(1.0f, 1.0f)) / 2.0f;
+				rightThumbstick -= Vector2(0.5f, 0.5f);
+				rightThumbstick *= 2.0f;
+
+				// creates vector toward joystick direction
+				float angle = atan2f(rightThumbstick.y, rightThumbstick.x);
+				Vector2 aimdirection = Vector2(cosf(angle), sinf(angle));
+
+				// normalizes vector and sets crosshair distance
+				aimdirection.Normalize();
+				aimdirection *= 150.0f;
+				ecs.getComponent<Sprite>(crosshair).enabled = true;
+				// crosshair position based on player position
+				Vector3 playerPosition(playerTransform.x, playerTransform.y, playerTransform.z);
+				Vector3 crosshairPosition = playerPosition + Vector3(aimdirection.x, -aimdirection.y, 0);
+				TransformSystem::SetPosition(crosshair, crosshairPosition);
+
+				if (fireCooldown <= 0)
+				{
+					if (axes[5] > 0.5f)
+					{
+
+						Entity bullet = ecs.newEntity();
+						ecs.addComponent(bullet, Transform{ .x = playerTransform.x + (aimdirection.x / 4), .y = playerTransform.y - (aimdirection.y / 4), .xScale = 5, .yScale = 5 });
+						ecs.addComponent(bullet, Sprite{ &texture3 });
+						ecs.addComponent(bullet, Rigidbody{ .velocity = Vector2(aimdirection.x * 40, -aimdirection.y * 40), .drag = 0, .elasticity = 0, .kinematic = true });
+						ecs.addComponent(bullet, BoxCollider{ .isTrigger = true });
+						bullets.push_back(bullet);
+						fireCooldown = 0.4f;
+					}
+				}
+				else
+				{
+					fireCooldown -= engine.deltaTime;
+				}
+
+				// set camera location between player and crosshair
+				//cam.SetPosition(playerTransform.x + (aimdirection.x / 8), playerTransform.y - (aimdirection.y / 8), playerTransform.z);
+			}
+			else
+			{
+				ecs.getComponent<Sprite>(crosshair).enabled = false;
+			}
+
+			for (const Entity& bullet : bullets)
+			{
+				auto hit = ecs.getComponent<BoxCollider>(bullet);
+				for (const Collision& collision : hit.collisions)
+				{
+					if (collision.type == Collision::Type::entityTrigger && collision.b != bullet)
+					{
+						ecs.destroyEntity(collision.b);
+						bullets.erase(std::remove(bullets.begin(), bullets.end(), bullet), bullets.end());
+						ecs.destroyEntity(bullet);
+					}
+				}
+			}
+
+			if (buttons[0] == GLFW_PRESS)
+			{
+				//tapahtuu jotain
+			}
+
 		}
-#endif
-	//	engine.physicsSystem->Move(player, Vector2(100, 100) * engine.deltaTime);
+
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 		//Update all engine systems, this usually should go last in the game loop
 		//For greater control of system execution, you can update each one manually
 		engine.Update(&cam);
 
-		//glClearColor(1, 0, 0, 1);
-		//glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+		map.draw();
+		cam.SetPosition(playerTransform.x, playerTransform.y, +100);
+
+		engine.Update(&cam);
 
 		//TODO add view matrix and get projection matrix from camera so that tilemap is rendered in the correct place
 		map.draw();
