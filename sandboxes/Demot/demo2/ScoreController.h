1df093bde7332762a1eec9ae0cd47fcaa3b674a7
Aaro Leskinen <aaroleskinen@kamk.fi>
2023-05-23 15:39:16 +0300

    ScoreCotroller.h for Scoreboard. Need to be checked does it work as meant to be

--- sandboxes/Demot/demo2/ScoreController.h
+++ sandboxes/Demot/demo2/ScoreController.h
@@ -3,16 +3,56 @@
 #include "PickupController.h";
 
 extern ECS ecs;
+
 using namespace engine;
+
+struct Scoreboard
+{
+	int victoryCondition = 0;
+};
 
 class ScoreController : public System
 {
 public:
-	ScoreController(int& collectedRef)
+	ScoreController(int& ref, const char* path, int spritesWide, int spritesHigh, int delays) : collectedRef(ref)
 	{
-		collectedRef = PickupController().collected;
-		std::cout << collectedRef << endl;
+		int spriteSize = spritesWide * spritesHigh;
+		anims = AnimationsFromSpritesheet(path, spritesWide, spritesHigh, vector<int>(spriteSize, delays));
+	};
+
+	void Update(Entity player)
+	{
+		AnimationSystem::AddAnimations(scoreboard, anims, vector<string>{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Victory"});
+		for (auto const& entity : entities)
+		{
+			BoxCollider& collider = ecs.getComponent<BoxCollider>(entity);
+
+				if (collider.collisions.end() == find_if(collider.collisions.begin(), collider.collisions.end(), [player](const Collision& collision) {return collision.a == player; }))
+				{
+					collectedRef = PickupController().collected;
+					if (collectedRef < anims.size())
+					{
+						string animation = to_string(collectedRef);
+						AnimationSystem::PlayAnimation(scoreboard, animation);
+					}
+					break;
+				}
+		}
 	}
 
+	Entity CreateScoreboard(float x, float y)
+	{
+		scoreboard = ecs.newEntity();
+		ecs.addComponent(scoreboard, Transform{ .x = x, .y = y, .xScale = 40, .yScale = 20 });
+		ecs.addComponent(scoreboard, Sprite{});
+		ecs.addComponent(scoreboard, Animator{});
+		ecs.addComponent(scoreboard, Scoreboard{});
 
+		return scoreboard;
+	}
+
+	Entity scoreboard;
+	vector<Animation> anims;
+	int& collectedRef;
+	const char* path;
 };
