e355205711ea8fea2b64dcf3085b4796d1a01397
Aleksi Anderson <aleksianderson@kamk.fi>
2023-05-25 15:28:09 +0300

    Cleaned up demo1 main

--- sandboxes/Demot/demo1/main.cpp
+++ sandboxes/Demot/demo1/main.cpp
@@ -71,15 +71,6 @@ int main()
 	turretControllerSignature.set(ecs.getComponentId<Animator>());
 	ecs.setSystemSignature<TurretController>(turretControllerSignature);
 
-	SoundSource waterSpeaker;
-	SoundSource torchSpeaker;
-	SoundSource torchSpeaker2;
-	SoundSource playerSpeaker;
-	SoundSource pickSpeaker;
-	SoundSource magicSpeaker;
-	MusicBuffer myMusic("assets/forest.wav");
-	myMusic.SetVolume(0.5f);
-	myMusic.Play();
 
 	//Create the player entity
 	Entity player = ecs.newEntity();
@@ -92,7 +83,6 @@ int main()
 	ecs.addComponent(player, Player{ .attackHitbox = playerAttack });
 
 	//Create the player's attack hitbox
-	//ecs.addComponent(playerAttack, Sprite{});
 	ecs.addComponent(playerAttack, Transform{ .xScale = 10, .yScale = 20 });
 	ecs.addComponent(playerAttack, Rigidbody{ .kinematic = true });
 	ecs.addComponent(playerAttack, BoxCollider{ .isTrigger = true });
@@ -103,59 +93,75 @@ int main()
 	vector<Animation> attackAnims = AnimationsFromSpritesheet("assets/warriorattack.png", 4, 4, vector<int>(4 * 4, 100));
 	AnimationSystem::AddAnimations(player, attackAnims, vector<string>{"Jump Attack", "Attack 1", "Attack 2", "Attack 3"});
 
+
+	//Make the waterfall sprites
+	Animation waterfallAnims = AnimationsFromSpritesheet("assets/waterfall01.png", 4, 1, vector<int>(4 * 1, 100))[0];
 	Entity waterfall = ecs.newEntity();
 	ecs.addComponent(waterfall, Transform{ .x = 1296, .y = -1410, .z = -10, .xScale = 110, .yScale = 50 });
 	ecs.addComponent(waterfall, Sprite{});
 	ecs.addComponent(waterfall, Animator{});
-	vector<Animation> waterfallAnims = AnimationsFromSpritesheet("assets/waterfall01.png", 4, 1, vector<int>(4 * 1, 100));
-	AnimationSystem::AddAnimation(waterfall, waterfallAnims[0], "1");
+	AnimationSystem::AddAnimation(waterfall, waterfallAnims, "1");
 	AnimationSystem::PlayAnimation(waterfall, "1", true);
 	Entity waterfall2 = ecs.newEntity();
 	ecs.addComponent(waterfall2, Transform{ .x = 1296, .y = -1507, .z = -15, .xScale = 110, .yScale = 50 });
 	ecs.addComponent(waterfall2, Sprite{});
 	ecs.addComponent(waterfall2, Animator{});
-	vector<Animation> waterfallAnims2 = AnimationsFromSpritesheet("assets/waterfall01.png", 4, 1, vector<int>(4 * 1, 100));
-	AnimationSystem::AddAnimation(waterfall2, waterfallAnims2[0], "1");
+	AnimationSystem::AddAnimation(waterfall2, waterfallAnims, "1");
 	AnimationSystem::PlayAnimation(waterfall2, "1", true);
 	Entity waterfall3 = ecs.newEntity();
 	ecs.addComponent(waterfall3, Transform{ .x = 1296, .y = -1604, .z = -15, .xScale = 110, .yScale = 50 });
 	ecs.addComponent(waterfall3, Sprite{});
 	ecs.addComponent(waterfall3, Animator{});
-	vector<Animation> waterfallAnims3 = AnimationsFromSpritesheet("assets/waterfall01.png", 4, 1, vector<int>(4 * 1, 100));
-	AnimationSystem::AddAnimation(waterfall3, waterfallAnims3[0], "1");
+	AnimationSystem::AddAnimation(waterfall3, waterfallAnims, "1");
 	AnimationSystem::PlayAnimation(waterfall3, "1", true);
 
+	//Make the static torches
+	Animation torchAnims = AnimationsFromSpritesheet("assets/torch.png", 9, 1, vector<int>(9 * 1, 40))[0];
 	Entity torch = ecs.newEntity();
 	ecs.addComponent(torch, Transform{ .x = 1480,  .y = -2036, .z = -10, .xScale = 15, .yScale = 35 });
 	ecs.addComponent(torch, Sprite{});
 	ecs.addComponent(torch, Animator{});
-	vector<Animation> torchAnims = AnimationsFromSpritesheet("assets/torch.png", 9, 1, vector<int>(9 * 1, 40));
-	AnimationSystem::AddAnimation(torch, torchAnims[0], "1");
-	AnimationSystem::PlayAnimation(torch, "1", true);
+	AnimationSystem::AddAnimation(torch, torchAnims, "main");
+	AnimationSystem::PlayAnimation(torch, "main", true);
 	Entity torch1 = ecs.newEntity();
 	ecs.addComponent(torch1, Transform{ .x = 850, .y = -2336, .z = -10, .xScale = 15, .yScale = 35 });
 	ecs.addComponent(torch1, Sprite{});
 	ecs.addComponent(torch1, Animator{});
-	vector<Animation> torchAnims1 = AnimationsFromSpritesheet("assets/torch.png", 9, 1, vector<int>(9 * 1, 40));
-	AnimationSystem::AddAnimation(torch1, torchAnims1[0], "1");
-	AnimationSystem::PlayAnimation(torch1, "1", true);
+	AnimationSystem::AddAnimation(torch1, torchAnims, "main");
+	AnimationSystem::PlayAnimation(torch1, "main", true);
 
-	RenderSystem::SetBackgroundColor(133 / 2, 117 / 2, 87 / 2);
 
+	//Load the tilemap
 	Tilemap map(&cam);
 	map.loadMap("assets/level01.tmx");
 	//Set the gravity and tilemap collider
 	engine.physicsSystem->gravity = Vector2(0, -10000);
 	engine.physicsSystem->SetTilemap(&map);
 	engine.renderSystem->SetTilemap(&map);
+	//Background color
+	RenderSystem::SetBackgroundColor(133 / 2, 117 / 2, 87 / 2);
 
+
+	//Sounds
+	//Load and play the music
+	MusicBuffer myMusic("assets/forest.wav");
+	myMusic.SetVolume(0.5f);
+	myMusic.Play();
+
+	//Create the speakers and set their distance models
+	SoundSource waterSpeaker;
+	SoundSource torchSpeaker;
+	SoundSource torchSpeaker2;
+	SoundSource playerSpeaker;
+	SoundSource pickSpeaker;
+	SoundSource magicSpeaker;
 	waterSpeaker.setLinearDistanceClamped(1.0f, 150.f, 700.f, 1.f);
 	torchSpeaker.setLinearDistanceClamped(0.5f, 50.f, 500.f, 1.f);
 	torchSpeaker2.setLinearDistanceClamped(0.5f, 50.f, 500.f, 1.f);
 	playerSpeaker.setLinearDistanceClamped(1.f, 50.f, 500.f, 1.f);
 	magicSpeaker.setLinearDistanceClamped(1.f, 50.f, 800.f, 1.f);
 
-
+	//Make all the strawberries
 	pickupController->CreatePickup(1880, -1100);
 	pickupController->CreatePickup(1010, -680);
 	pickupController->CreatePickup(720, -1510);
@@ -172,7 +178,7 @@ int main()
 	turretController->CreateTurret(1590, -2360);
 	turretController->CreateTurret(1790, -2640);
 
-
+	//Load all the sounds
 	uint32_t waterSound = SoundBuffer::getFile()->addSoundEffect("assets/stream.wav");
 	uint32_t torchSound = SoundBuffer::getFile()->addSoundEffect("assets/fire.wav");
 	uint32_t pickSound = SoundBuffer::getFile()->addSoundEffect("assets/strawberry_touch.wav");
@@ -180,33 +186,32 @@ int main()
 	uint32_t attackSound = SoundBuffer::getFile()->addSoundEffect("assets/swing.wav");
 	uint32_t magicSound = SoundBuffer::getFile()->addSoundEffect("assets/Magic.wav");
 
+	//Set all static sounds to always play
 	waterSpeaker.Play(waterSound);
 	waterSpeaker.SetLooping(1);
-
 	torchSpeaker.Play(torchSound);
 	torchSpeaker.SetLooping(1);
 	torchSpeaker2.Play(torchSound);
 	torchSpeaker2.SetLooping(1);
+	//Set all static sound locations
+	engine.soundDevice->SetSourceLocation(waterSpeaker, 1296, -1556, -35);
+	engine.soundDevice->SetSourceLocation(torchSpeaker, 1480, -2036, 1);
+	engine.soundDevice->SetSourceLocation(torchSpeaker2, 850, -2336, 1);
+
 	//Game Loop
 	while (!glfwWindowShouldClose(window))
 	{
-		//std::this_thread::sleep_for(std::chrono::milliseconds{100});
-
 		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 			glfwSetWindowShouldClose(window, true);
 
-		if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS)
-		{
-			cout << playerTransform.x << ", " << playerTransform.y << endl;
-		}
-		myMusic.updateBufferStream();
+		//Update custom systems
 		playerController->Update(window, engine.deltaTime);
 		pickupController->Update(player, engine.programTime);
 		turretController->Update(engine.deltaTime);
 
-		//Update all engine systems, this usually should go last in the game loop
-		//For greater control of system execution, you can update each one manually
+		//Update all engine systems
 		engine.Update(&cam);
+
 
 		//Keep the camera in bounds of the tilemap
 		float camPosX = clamp(playerTransform.x, map.position.x + cam.width / 2, map.position.x + map.bounds.width - cam.width / 2);
@@ -214,73 +219,73 @@ int main()
 		cam.SetPosition(camPosX, camPosY, 100);
 
 
-		engine.soundDevice->SetOrientation(0.f, 1.f, 0.f, 0.f, 0.f, 1.f);
-
-		engine.soundDevice->SetLocation(playerTransform.x, playerTransform.y, 0);
-
-		for (Entity entity : pickupController->entities)
+		//Sounds unfortunately must be done in main for now
 		{
-			Pickup& pickup = ecs.getComponent<Pickup>(entity);
-			if (pickup.sound == 1)
+			myMusic.updateBufferStream();
+			engine.soundDevice->SetOrientation(0.f, 1.f, 0.f, 0.f, 0.f, 1.f);
+			engine.soundDevice->SetLocation(playerTransform.x, playerTransform.y, 0);
+
+			//Set player speaker location
+			engine.soundDevice->SetSourceLocation(playerSpeaker, playerTransform.x, playerTransform.y, 1);
+
+			//Player jump and sword
+			for (Entity entity : playerController->entities)
 			{
-				if (!pickSpeaker.isPlaying())
-					pickSpeaker.Play(pickSound);
-				pickup.sound = 0;
-			}
-		};
+				Player& player = ecs.getComponent<Player>(entity);
 
+				if (player.attackSound == 1)
+				{
 
-		engine.soundDevice->SetSourceLocation(waterSpeaker, 1296, -1556, -35);
-		engine.soundDevice->SetSourceLocation(torchSpeaker, 1480, -2036, 1);
-		engine.soundDevice->SetSourceLocation(torchSpeaker2, 850, -2336, 1);
-		engine.soundDevice->SetSourceLocation(playerSpeaker, playerTransform.x, playerTransform.y, 1);
+					playerSpeaker.Play(attackSound);
+					player.attackSound = 0;
+				}
+				if (player.jumpSound == 1)
+				{
+					if (!playerSpeaker.isPlaying())
+						playerSpeaker.Play(jumpSound);
+					player.jumpSound = 0;
+				}
+			};
 
-
-		for (Entity entity : playerController->entities)
-		{
-			Player& player = ecs.getComponent<Player>(entity);
-			if (player.jumpSound == 1)
+			//Pickup collect sound
+			for (Entity entity : pickupController->entities)
 			{
-				if (!playerSpeaker.isPlaying())
-					playerSpeaker.Play(jumpSound);
-				player.jumpSound = 0;
-			}
-		};
-		for (Entity entity : playerController->entities)
-		{
-			Player& player = ecs.getComponent<Player>(entity);
-			if (player.attackSound == 1)
+				Pickup& pickup = ecs.getComponent<Pickup>(entity);
+
+				if (pickup.sound == 1)
+				{
+					if (!pickSpeaker.isPlaying())
+						pickSpeaker.Play(pickSound);
+					pickup.sound = 0;
+				}
+			};
+
+			//Play the magic sound from nearest enemy
+			if (turretController->playSound)
 			{
+				//Find the closest enemy
+				Entity closestEnemy = 0;
+				for (Entity entity : turretController->entities)
+				{
+					if (!ecs.entityExists(closestEnemy))
+					{
+						closestEnemy = entity;
+						continue;
+					}
 
-				playerSpeaker.Play(attackSound);
-				player.attackSound = 0;
+					if (TransformSystem::Distance(player, entity) < TransformSystem::Distance(player, closestEnemy))
+						closestEnemy = entity;
+				};
+
+				Transform closestEnemyTransform = ecs.getComponent<Transform>(closestEnemy);
+
+				engine.soundDevice->SetSourceLocation(magicSpeaker, closestEnemyTransform.x, closestEnemyTransform.y, 0);
+
+				turretController->playSound = false;
+				if (!magicSpeaker.isPlaying())
+					magicSpeaker.Play(magicSound);
 			}
-		};
-
-		Entity closestEnemy = 0;
-		for (Entity entity : turretController->entities)
-		{
-			if (!ecs.entityExists(closestEnemy))
-			{
-				closestEnemy = entity;
-				continue;
-			}
-
-			if (TransformSystem::Distance(player, entity) < TransformSystem::Distance(player, closestEnemy))
-				closestEnemy = entity;
-		};
-
-		if (ecs.entityExists(closestEnemy) && turretController->playSound)
-		{
-			Transform closestEnemyTransform = ecs.getComponent<Transform>(closestEnemy);
-
-			engine.soundDevice->SetSourceLocation(magicSpeaker, closestEnemyTransform.x, closestEnemyTransform.y, 0);
-
-			turretController->playSound = false;
-			if(!magicSpeaker.isPlaying())
-				magicSpeaker.Play(magicSound);
 		}
-
 
 		//OpenGL stuff, goes very last
 		glfwSwapBuffers(window);
