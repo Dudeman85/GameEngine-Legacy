f8ab37a8e8d09e618573c3d5e3246f50d6f9fb79
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2023-08-11 12:18:41 +0300

    Added some code but not working yet. Need to fix

--- include/engine/TextRender.h
+++ include/engine/TextRender.h
@@ -1,9 +1,30 @@
 #pragma once
 
+#include <iostream>
+
+using namespace std;
+
 class TextRender
 {
 public:
+
+	// Constructor
+	//TextRender(engine::Camera* cam);
+
+	//Destructor
+	~TextRender();
+
 	// face_index points to the font in the file if file has
 	// multiple fonts. In this case index is 0
 	void FontLoad(const char* filepathname, FT_Long face_index);
+
+	// Text render function that renders a string of characters
+	//void RenderText(string text, float x, float y, float scale, glm::vec3 color);
+
+private:
+
+	void initGLStuff();
+
+	//engine::Shader* m_shader;
+	//engine::Camera* camera;
 };
--- src/TextRender.cpp
+++ src/TextRender.cpp
@@ -4,15 +4,25 @@
 #include <engine/TextRender.h>
 
 #include <iostream>
+#include <engine/GL/Shader.h>
 #include <engine/Application.h>
+#include <engine/GL/Camera.h>
 #include <map>
 
 using namespace std;
 
-void TextRender::FontLoad(const char* filepathname, FT_Long face_index)
-{
+//TextRender::TextRender(engine::Camera* cam)
+//{
+	//m_shader = new engine::Shader("vertexShader.glsl", "fragmentShader.glsl");
+	//camera = cam;
+//}
 
-	
+TextRender::~TextRender()
+{
+}
+
+void TextRender::FontLoad(const char* filepathname, FT_Long face_index)
+{	
 	// Initialize FreeType library
 	FT_Library ft;
 	if (FT_Init_FreeType(&ft))
@@ -121,5 +131,69 @@ void TextRender::FontLoad(const char* filepathname, FT_Long face_index)
 	// Clearing FreeTypes resources
 	FT_Done_Face(face);
 	FT_Done_FreeType(ft);
+
+	// configure VAO/VBO for texture quads
+   // -----------------------------------
+	//glGenVertexArrays(1, &VAO);
+	//glGenBuffers(1, &VBO);
+	//glBindVertexArray(VAO);
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
+
+void TextRender::initGLStuff()
+{
+
+}
+
+//void TextRender::RenderText(string text, float x, float y, float scale, glm::vec3 color)
+//{
+	// active corresponding render state
+	//m_shader->use();
+	//glUniform3f(glGetUniformLocation(m_shader->ID, "textColor"), color.x, color.y, color.z);
+	//glActiveTexture(GL_TEXTURE0);
+	//glBindVertexArray(VAO);
+
+	// iterate through all characters
+	//string::const_iterator c;
+
+	//for(c = text.begin(); c != text.end(); c++)
+	//{
+		//Character ch = Characters[*c];
+
+		//float xpos = x + ch.Bearing.x * scale;
+		//float yps = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		//float w = ch.Size.x * scale;
+		//float h = ch.Size.y * scale;
+		// update VBO for each character
+		//float vertices[6][4] = {
+			//{ xpos,     ypos + h,   0.0f, 0.0f },
+			//{ xpos,     ypos,       0.0f, 1.0f },
+			//{ xpos + w, ypos,       1.0f, 1.0f },
+
+			//{ xpos,     ypos + h,   0.0f, 0.0f },
+			//{ xpos + w, ypos,       1.0f, 1.0f },
+			//{ xpos + w, ypos + h,   1.0f, 0.0f }
+		//};
+		// render glyph texture over quad
+		//glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// update content of VBO memory
+		//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		//glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+		//glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// render quad
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		//x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+	//}
+	//glBindVertexArray(0);
+	//glBindTexture(GL_TEXTURE_2D, 0);
+	//}
+//}
 
