a42b30e5b19585f780c15b907f015bd9a3438b3c
Aaro Leskinen <aaro.leskinen@kolumbus.fi>
2023-08-30 23:00:33 +0300

    Simple TextRendering now works in TestScene. All code is in main. Need to still figure out how to insert it to our project.

--- bin/textFragmentShader.glsl
+++ bin/textFragmentShader.glsl
@@ -1,12 +1,12 @@
 #version 330 core
 in vec2 TexCoords;
-out vec4 color;
+out vec4 colour;
 
 uniform sampler2D text;
-uniform vec3 textColor;
+uniform vec3 textColour;
 
 void main()
 {    
     vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, TexCoords).r);
-    color = vec4(textColor, 1.0) * sampled;
+    colour = vec4(textColour, 1.0) * sampled;
 }
