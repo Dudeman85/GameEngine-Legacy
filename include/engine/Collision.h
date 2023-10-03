979fdddf1479e5196d0c2bb9ed0b46a2714fefcb
Anderson Aleksi TTV21SP <aleksianderson@kamk.fi>
2023-10-03 15:07:43 +0300

    Further implemented SAT Collision

--- include/engine/Collision.h
+++ include/engine/Collision.h
@@ -14,10 +14,13 @@ namespace engine
 		//The vertices of the polygon making up the collider, going clockwise
 		//The vertices must form a convex polygon
 		std::vector<Vector2> vertices;
-		
+
 		//The axis-aligned bounding box of the collider
 		//This is updated automatically and is set in world coordinates
 		std::array<float, 4> bounds;
+
+		//Draw the collider vertices
+		bool visualize = false;
 	};
 
 	//Collision Event struct, not a component
@@ -47,25 +50,113 @@ namespace engine
 			}
 		}
 
-		//Checks collision between entity and every other entity
-		vector<CollisionEvent> CheckCollision(Entity entity)
+		//Checks collision between entity a and every other entity
+		//Returns the collisions from the perspective of a
+		vector<CollisionEvent> CheckCollision(Entity a)
 		{
-			Transform& transform = ecs.getComponent<Transform>(entity);
-			PolygonCollider& collider = ecs.getComponent<PolygonCollider>(entity);
+			Transform& aTransform = ecs.getComponent<Transform>(a);
+			PolygonCollider& aCollider = ecs.getComponent<PolygonCollider>(a);
+
+			//Rotate and scale every vertex of a, movement is handled later
+			vector<Vector2> aVerts;
+			for (int i = 0; i < aCollider.vertices.size(); i++)
+			{
+				Vector2 transformedVert = aCollider.vertices[i];
+				//Rotate
+				float angle = aTransform.rotation.z * PI / 180;
+				transformedVert.x = aCollider.vertices[i].x * cos(angle) - aCollider.vertices[i].y * sin(angle);
+				transformedVert.y = aCollider.vertices[i].x * sin(angle) + aCollider.vertices[i].y * cos(angle);
+				//Scale
+				transformedVert *= Vector2(aTransform.scale);
+				aVerts.push_back(transformedVert);
+			}
+
+			//Store every collision between this entity and others
+			std::vector<CollisionEvent> collisions;
 
 			//For each entity
-			for (const Entity& entity2 : entities)
+			for (const Entity& b : entities)
 			{
 				//Don't collide with self
-				if (entity == entity2)
+				if (a == b)
 					continue;
 
 				//Check AABB collision first
-				if (!AABBIntersect(entity, entity2))
+				if (!AABBIntersect(a, b))
 					continue;
+
+				Transform& bTransform = ecs.getComponent<Transform>(b);
+				PolygonCollider& bCollider = ecs.getComponent<PolygonCollider>(b);
+
+				//Rotate and scale every vertex of b, movement is handled later
+				vector<Vector2> bVerts;
+				for (int i = 0; i < bCollider.vertices.size(); i++)
+				{
+					Vector2 transformedVert = bCollider.vertices[i];
+					//Rotate
+					float angle = bTransform.rotation.z * PI / 180;
+					transformedVert.x = bCollider.vertices[i].x * cos(angle) - bCollider.vertices[i].y * sin(angle);
+					transformedVert.y = bCollider.vertices[i].x * sin(angle) + bCollider.vertices[i].y * cos(angle);
+					//Scale
+					transformedVert *= Vector2(bTransform.scale);
+					bVerts.push_back(transformedVert);
+				}
+
+				//Check collision from both a and b perspectives
+				CollisionEvent aToB = SATIntersect(aVerts, bVerts, a, b);
+				CollisionEvent bToA;
+				//If a to b is miss no need to check b to a
+				if (aToB.type != CollisionEvent::Type::miss)
+				{
+					bToA = SATIntersect(bVerts, aVerts, b, a);
+					if (bToA.type != CollisionEvent::Type::miss)
+						collisions.push_back(aToB);
+				}
+			}
+			return collisions;
+		}
+
+		//Check SAT intersection
+		//Expects Vertices to have Rotation and Scale applied, but not Position, this if for optimization
+		CollisionEvent SATIntersect(vector<Vector2> aVerts, vector<Vector2> bVerts, Entity a, Entity b)
+		{
+			Vector2 aPosition = ecs.getComponent<Transform>(a).position;
+			Vector2 bPosition = ecs.getComponent<Transform>(b).position;
+
+			//For each vertice in b move it to have a at origin
+			for (int i = 0; i < bVerts.size(); i++)
+			{
+				bVerts[i] += bPosition;
+				bVerts[i] -= aPosition;
 			}
 
-			return std::vector<CollisionEvent>();
+			//For each vertice in a
+			for (int i = 0; i < aVerts.size(); i++)
+			{
+				//Overflow nextVertice to beginning
+				int nextVertice = i < aVerts.size() - 1 ? i + 1 : 0;
+				//Calculate the normal vector to project the other polygon to (left normal because clockwise)
+				Vector2 normal = (aVerts[nextVertice] - aVerts[i]).LeftNormal();
+				//The boundry of the polygon projected to normal
+				float boundary = normal.Dot(aVerts[i]);
+
+				float miss = true;
+
+				//For each vertice in b
+				for (int j = 0; j < bVerts.size(); j++)
+				{
+					//Project the vertice of b to the normal of the current a vertice
+					float projection = normal.Dot(bVerts[j]);
+					//If the projection of the vertex is inside the boundary
+					if (abs(projection) < abs(boundary))
+					{
+						miss = false;
+					}
+				}
+				if (miss)
+					return CollisionEvent(); //False
+			}
+			return CollisionEvent(); //true;
 		}
 
 		//Checks if a and b bounds are intersecting
@@ -86,7 +177,7 @@ namespace engine
 			PolygonCollider& collider = ecs.getComponent<PolygonCollider>(entity);
 
 			//Bounds go top, right, bottom, left
-			std::array<float, 4> bounds{-INFINITY, -INFINITY, INFINITY, INFINITY};
+			std::array<float, 4> bounds{ -INFINITY, -INFINITY, INFINITY, INFINITY };
 			//For each vertice apply the tranform and calculate min and max bounds
 			for (int i = 0; i < collider.vertices.size(); i++)
 			{
@@ -115,7 +206,7 @@ namespace engine
 				if (transformedVert.x < bounds[3])
 					bounds[3] = transformedVert.x;
 			}
-			
+
 			collider.bounds = bounds;
 		}
 	};
